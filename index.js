830fd916a5a3a67329ddcd892e3bf405eca5dc78
yusuf29 <ysufali9999@gmail.com>
2024-06-05 01:26:05 +0600

    jwt verify route done

--- index.js
+++ index.js
@@ -4,9 +4,25 @@ const cors = require("cors")
 require("dotenv").config()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken")
 
 app.use(cors())
 app.use(express.json())
+
+const TokenVerify = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ message: "unauthorized access" })
+    }
+    const token = authorization.split(" ")[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "unauthorized access" })
+        }
+        req.decoded = decoded
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qud1tkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,9 +42,23 @@ async function run() {
 
         const userCollection = client.db("planetshoes").collection("user")
 
-        app.post("/user",async(req,res)=>{
-            const user=req.body;
-            const result=await userCollection.insertOne(user)
+
+        app.post("/jwt", async (req, res) => {
+            const userData = req.body;
+            const token = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" })
+            res.send({ token })
+        })
+
+
+        //new user saved on database
+        app.post("/newUser", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: "user already exists" })
+            }
+            const result = await userCollection.insertOne(user)
             res.send(result)
         })
 
